b0d067353f7617df825c583ee6e643ecf4365e18
vjetfpt <vietbhph09726@fpt.edu.vn>
2021-03-19 09:51:08 +0700

    vua hoc it state

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Header from './components/Header/index'
 import AlbumExample from './components/Album_Example';
 import Album from './components/Album';
 import Footer from './components/Footer';
+import React from 'react'
 function formatDate(date){
   return date.toLocaleDateString();
 }
@@ -35,6 +36,30 @@ const comment={
     'https://placekitten.com/g/64/64',
   }
 }
+class Form extends React.Component {
+  constructor (props) {
+     super(props)
+     this.state = {
+       input: ''
+     }
+  }
+handleChange = (text) => {
+    this.setState({ input: text })
+  }
+  
+  render () {
+    const { input } = this.state
+    return (
+    <div>
+        <label>
+          Name:
+           <input type="text" value={this.state.value} onChange={this.handleChange} />
+        </label>
+        <input type="submit" value="Submit" />
+      </div>
+      )
+    }
+ }
 function App() {
   return (
     <div className="App">
@@ -42,6 +67,7 @@ function App() {
       <main>
         <AlbumExample />
         <UserInfo user={comment.author}/>
+        <Form />
         <Album />
       </main>
       <Footer />
